b7c0582cf889483b581011264e33a56b8850c3e9
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-10-08 00:49:12 +0200

    Patch test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/https://kevin-jerbic.github.io/Kaza/',
+    base: '/https://kevin-jerbic.github.io/Kaza/src/main.jsx',
 });
