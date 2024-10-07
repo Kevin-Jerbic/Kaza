deb82adad3dd392ddb272dd539fe87b7a9c240e2
Kevin-Jerbic <94851418+Kevin-Jerbic@users.noreply.github.com>
2024-10-08 00:02:34 +0200

    Add fonts, images and textes

---
+++ src/assets/images/img.js
@@ -0,0 +1,23 @@
+import logoHeader from './logo_header.svg';
+import logoFooter from './logo_footer.svg';
+import arrowBack from './arrow_back.svg';
+import arrowNext from './arrow_next.svg';
+import arrowUp from './arrow_up.svg';
+import arrowDown from './arrow_down.svg';
+import starOrange from './star_orange.svg';
+import starLight from './star_light.svg';
+import bannerHome from './banner_home.png';
+import bannerAbout from './banner_about.png';
+
+export const IMAGES = {
+    logoHeader,
+    logoFooter,
+    arrowBack,
+    arrowNext,
+    arrowUp,
+    arrowDown,
+    starOrange,
+    starLight,
+    bannerHome,
+    bannerAbout
+};
---
+++ src/assets/textes/constants.js
@@ -0,0 +1,25 @@
+export const STRINGS = {
+    HEADER_MENU_HOME: 'Accueil',
+    HEADER_MENU_ABOUT: 'À propos',
+    BANNER_TITLE: 'Chez vous, partout et ailleurs',
+    COPYRIGHT: '© 2020 Kasa. All rights reserved',
+};
+
+export const ABOUT_TXT = {
+    first: {
+        title: 'Fiabilité',
+        text: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+    },
+    second: {
+        title: 'Respect',
+        text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    third: {
+        title: 'Service',
+        text: 'La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce quie chaque interaction, que cec soit avec nos hôtes ou nos locatrices, soit empreinte de respect et de bienveillance.',
+    },
+    fourth: {
+        title: 'Sécurité',
+        text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+};
